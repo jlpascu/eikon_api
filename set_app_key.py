93deb6f88cdb92daa9e19b2992ba6030547c3d79
jlpascu <69301150+jlpascu@users.noreply.github.com>
2021-12-19 13:53:26 +0100

    Update set_app_key.py
    Make sure you have created you App Key before starting your code. For this type App Key Generator on the Eikon browser bar and create new keye.  Once your have created, use the following code to set your API key (it is not necessary to specifically copy&plaste your App Key)

--- set_app_key.py
+++ set_app_key.py
@@ -1,7 +1,2 @@
-# Make sure you have created you App Key before starting your code. 
-# For this type App Key Generator on the Eikon browser bar and create new keye. 
-# Once your have created, use the following code to set your API key
-# (it is not necessary to specifically copy&plaste your App Key)
-
 # Set Eikon App Key
 ek.set_app_key('DEFAULT_CODE_BOOK_APP_KEY')
